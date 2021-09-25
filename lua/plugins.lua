6f12864c894d09c6fe0257ebca90a9eec063cb3a
henryw <henrywang97@gmail.com>
2021-09-25 18:27:01 +0100

    added packer bootstrap

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,3 +1,11 @@
+-- install packer if not found
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+    os.execute(string.format("git clone --depth 1 https://github.com/wbthomason/packer.nvim %s", install_path))
+    vim.cmd 'packadd packer.nvim'
+end
+
 local not_windows = require('utils').not_windows
 
 return require('packer').startup(function()
