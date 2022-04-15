106edf2106b2c43afa9e6317af2f1fc4b78ef15d
Henry Wang <henrywang97@gmail.com>
2022-04-15 20:53:24 +0100

    add focus maximise keymap

--- lua/config/focus.lua
+++ lua/config/focus.lua
@@ -3,3 +3,4 @@ require("focus").setup({
 })
 
 vim.api.nvim_set_keymap('n', '<Del>', ':FocusToggle<CR>', { noremap=true, silent=true })
+vim.api.nvim_set_keymap('n', '<C-Del>', ':FocusMaximise<CR>', { noremap=true, silent=true })
