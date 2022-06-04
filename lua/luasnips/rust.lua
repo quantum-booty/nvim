56a0a929e6a7ba359b10a6939de9284e9e5badfe
Henry Wang <henrywang97@gmail.com>
2022-06-04 23:49:17 +0100

    add match_some snippet

---
+++ lua/luasnips/rust.lua
@@ -0,0 +1,34 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local sn = ls.snippet_node
+local isn = ls.indent_snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+local r = ls.restore_node
+local events = require("luasnip.util.events")
+local ai = require("luasnip.nodes.absolute_indexer")
+local fmt = require("luasnip.extras.fmt").fmt
+local m = require("luasnip.extras").m
+local lambda = require("luasnip.extras").l
+
+
+local match_result = s(
+    "matchr",
+    fmt([[
+    match {1} {{
+        Ok({2}) => {3},
+        Err({4}) => {5},
+    }};
+    ]], {
+        i(1, "expr"),
+        i(2, "result"),
+        i(3, ""),
+        i(4, "error"),
+        i(5, ""),
+    }))
+
+
+return { match_result }
