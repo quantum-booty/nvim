f3342d977835ce95ac7e7c9a2327735fdbd769e4
quantum-booty <henrywang97@gmail.com>
2022-06-07 23:08:01 +0100

    delete more unused configs

--- ftplugin/cs_mappings.lua
+++
@@ -1,6 +0,0 @@
-local map = require('utils').map
-local buf_map = require('utils').buf_map
-local opts = { noremap=true, silent=true}
-
-
-buf_map(0, 'n', '<c-r>', '<cmd>TermExec cmd="dotnet run"<cr>', opts)
