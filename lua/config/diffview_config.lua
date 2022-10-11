75d9d4b16a23cb8614187169ca73a8549d7ef6ef
Henry Wang <henrywang97@gmail.com>
2022-10-11 20:04:43 +0100

    add gitsigns and diffview mappings

--- lua/config/diffview_config.lua
+++ lua/config/diffview_config.lua
@@ -1,9 +1,24 @@
+local map = require('utils').map
+local opts = { noremap = true, silent = true }
+
+vim.api.nvim_create_user_command("DiffviewToggle", function(e)
+  local view = require("diffview.lib").get_current_view()
+
+  if view then
+    vim.cmd("DiffviewClose")
+  else
+    vim.cmd("DiffviewOpen " .. e.args)
+  end
+end, { nargs = "*" })
+
+
 require("diffview").setup({
   enhanced_diff_hl = true,
   keymaps = {
     file_panel = {
       ["<down>"]        = false,
       ["<up>"]          = false,
+      ["<leader>D"]     = ":DiffviewToggle<cr>",
     },
     file_history_panel = {
       ["<down>"]        = false,
@@ -11,3 +26,6 @@ require("diffview").setup({
     },
   },
 })
+
+
+map('n', '<leader>D', ':DiffviewToggle<cr>', opts)
