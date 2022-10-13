98a28692f38a7686f262c77047257f08ccf30d42
Henry Wang <henrywang97@gmail.com>
2022-10-13 16:22:01 +0100

    noice

--- lua/config/noice_config.lua
+++ lua/config/noice_config.lua
@@ -14,7 +14,7 @@ require("noice").setup(
             -- NOTE: If you enable noice messages UI, noice cmdline UI is enabled
             -- automatically. You cannot enable noice messages UI only.
             -- It is current neovim implementation limitation.  It may be fixed later.
-            enabled = false, -- disable if you use native messages UI
+            enabled = true, -- disable if you use native messages UI
         },
         popupmenu = {
             enabled = false, -- disable if you use something like cmp-cmdline
@@ -34,7 +34,7 @@ require("noice").setup(
             -- event is always "notify" and kind can be any log level as a string
             -- The default routes will forward notifications to nvim-notify
             -- Benefit of using Noice for this is the routing and consistent history view
-            enabled = false,
+            enabled = true,
         },
         hacks = {
             -- due to https://github.com/neovim/neovim/issues/20416
@@ -44,7 +44,7 @@ require("noice").setup(
             -- Enable this option to simply skip duplicate messages instead.
             skip_duplicate_messages = false,
         },
-        throttle = 1000 / 30, -- how frequently does Noice need to check for ui updates? This has no effect when in blocking mode.
+        -- throttle = 1000 / 30, -- how frequently does Noice need to check for ui updates? This has no effect when in blocking mode.
         ---@type table<string, NoiceViewOptions>
         views = {}, -- @see the section on views below
         ---@type NoiceRouteConfig[]
