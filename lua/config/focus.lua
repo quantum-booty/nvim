960540087056b72435e0a9e1ca45c860a38b4b77
Henry Wang <henrywang97@gmail.com>
2022-10-14 21:53:56 +0100

    delete unused config

--- lua/config/focus.lua
+++
@@ -1,6 +0,0 @@
-require("focus").setup({
-    signcolumn = false
-})
-
-vim.api.nvim_set_keymap('n', '<Del>', ':FocusToggle<CR>', { noremap=true, silent=true })
-vim.api.nvim_set_keymap('n', '<C-Del>', ':FocusMaximise<CR>', { noremap=true, silent=true })
