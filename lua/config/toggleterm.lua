716374683df4117cc06247a2f69c1bd1fcad99f9
Henry Wang <henrywang97@gmail.com>
2022-10-15 11:52:33 +0100

    add lazydocker

--- lua/config/toggleterm.lua
+++ lua/config/toggleterm.lua
@@ -1,4 +1,6 @@
+require("toggleterm").setup()
 local Terminal  = require('toggleterm.terminal').Terminal
+
 local lazygit = Terminal:new(
     {
         cmd = "lazygit",
@@ -19,11 +21,37 @@ local lazygit = Terminal:new(
         },
     })
 
-require("toggleterm").setup()
-
-function _lazygit_toggle()
+local function _lazygit_toggle()
   lazygit:toggle()
 end
 
-vim.api.nvim_set_keymap("n", "<c-t>", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("t", "<c-t>", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+vim.keymap.set("n", "<c-t>", _lazygit_toggle, {noremap = true, silent = true})
+vim.keymap.set("t", "<c-t>", _lazygit_toggle, {noremap = true, silent = true})
+
+
+local lazydocker = Terminal:new(
+    {
+        cmd = "lazydocker",
+        hidden = true,
+        direction = "float",
+        on_open = function()
+            if vim.fn.has 'win32' == 1 then
+                vim.cmd([[startinsert]])
+            end
+        end,
+        float_opts = {
+            width = function()
+                return vim.o.columns
+            end,
+            height = function()
+                return vim.o.lines
+            end,
+        },
+    })
+
+local function _lazydocker_toggle()
+  lazydocker:toggle()
+end
+
+vim.keymap.set("n", "<c-r>", _lazydocker_toggle, {noremap = true, silent = true})
+vim.keymap.set("t", "<c-r>", _lazydocker_toggle, {noremap = true, silent = true})
