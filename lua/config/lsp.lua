d99de2eaf5c08c2555e17b46f3b463b3ecae9a6a
Henry Wang <henrywang97@gmail.com>
2022-10-15 11:47:26 +0100

    fix cmp

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -122,7 +122,7 @@ end
 -------------------------------------------------------------------------------
 -- nvim-cmp settings
 -------------------------------------------------------------------------------
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 
 
