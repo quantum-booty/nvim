6ff8c2081ad4f21e00ab8848f9901f89de3577a6
quantum-booty <henrywang97@gmail.com>
2022-10-16 15:03:22 +0100

    fix cmp defalut capabilities

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -121,7 +121,7 @@ end
 -------------------------------------------------------------------------------
 -- nvim-cmp settings
 -------------------------------------------------------------------------------
-local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 -------------------------------------------------------------------------------
 -- Mason
