2210c600b8c3df874ee9d31be62297180e68fa21
Henry Wang <henrywang97@gmail.com>
2022-10-21 19:01:21 +0100

    refactor

--- lua/config/copilot.lua
+++ lua/config/copilot.lua
@@ -1,21 +1,18 @@
-local not_windows = require('utils').not_windows
-if not_windows() then
-    vim.cmd([[
-        let g:copilot_node_command = "~/.nvm/versions/node/v17.9.1/bin/node"
+vim.cmd([[
+    let g:copilot_node_command = "~/.nvm/versions/node/v17.9.1/bin/node"
 
-        imap <silent><script><expr> <right> copilot#Accept("\<right>")
-        let g:copilot_no_tab_map = v:true
-        let g:copilot_filetypes = {
-              \ '*': v:false,
-              \ 'python': v:true,
-              \ 'rust': v:true,
-              \ 'cs': v:true,
-              \ }
+    imap <silent><script><expr> <right> copilot#Accept("\<right>")
+    let g:copilot_no_tab_map = v:true
+    let g:copilot_filetypes = {
+          \ '*': v:false,
+          \ 'python': v:true,
+          \ 'rust': v:true,
+          \ 'cs': v:true,
+          \ }
 
-        augroup copilot
-            autocmd FileType python Copilot setup
-            autocmd FileType rust Copilot setup
-            autocmd FileType cs Copilot setup
-        augroup end
-    ]])
-end
+    augroup copilot
+        autocmd FileType python Copilot setup
+        autocmd FileType rust Copilot setup
+        autocmd FileType cs Copilot setup
+    augroup end
+]])
