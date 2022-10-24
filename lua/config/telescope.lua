6890b11d0be751592a581307e0231284ba5b87d5
Henry Wang <henrywang97@gmail.com>
2022-10-24 20:13:22 +0100

    fallback to find_files if not a git repo

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -1,5 +1,5 @@
 local map = require('utils').map
-local opts = { noremap=true, silent=true }
+local opts = { noremap = true, silent = true }
 
 local actions = require("telescope.actions")
 local trouble = require("trouble.providers.telescope")
@@ -31,9 +31,9 @@ end
 
 
 
-telescope.setup{
+telescope.setup {
     defaults = {
-        path_display={"smart"},
+        path_display = { "smart" },
         sorting_strategy = "ascending",
         layout_config = {
             horizontal = {
@@ -63,11 +63,11 @@ telescope.setup{
         git_files = { theme = "dropdown" },
         buffers = { theme = "dropdown" },
         colorscheme = { theme = "dropdown" },
-        help_tags = { jump_type = 'tab'},
+        help_tags = { jump_type = 'tab' },
         current_buffer_fuzzy_find = { theme = "dropdown" },
-        lsp_definitions = { initial_mode='normal', theme = "ivy", show_line = false },
-        lsp_implementations = { initial_mode='normal',  theme = "ivy", show_line = false },
-        lsp_references = { initial_mode='normal', theme = "ivy", show_line = false },
+        lsp_definitions = { initial_mode = 'normal', theme = "ivy", show_line = false },
+        lsp_implementations = { initial_mode = 'normal', theme = "ivy", show_line = false },
+        lsp_references = { initial_mode = 'normal', theme = "ivy", show_line = false },
     },
     extensions = {
         -- file_browser = {
@@ -98,22 +98,30 @@ telescope.setup{
     },
 }
 
+local project_files = function()
+    local _, ret, _ = require('telescope.utils').get_os_command_output({ 'git', 'rev-parse', '--is-inside-work-tree' })
+    if ret == 0 then
+        require('telescope.builtin').git_files()
+    else
+        require('telescope.builtin').find_files()
+    end
+end
 
-map('n', '_', '<leader>p', { noremap=false, silent=true })
+
+map('n', '_', '<leader>p', { noremap = false, silent = true })
 
 -- " project_nvim
 map('n', '<leader>pp', ':Telescope projects<CR>', opts)
 
 -- " --- File Pickers
 -- " Fuzzy find over git files in your directory
-vim.keymap.set('n', '<C-p>', require('telescope.builtin').git_files, opts)
+vim.keymap.set('n', '<C-p>', project_files, opts)
 vim.keymap.set('n', '<leader>ps', require('telescope.builtin').git_status, opts)
--- " Search over files in your cwd current working directory.
-map('n', '<leader>pf', [[<cmd>lua require('telescope.builtin').find_files()<CR>]], opts)
 -- " grep typed string
 map('n', '<leader>pg', [[<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>]], opts)
 -- " grep cursor word
-map('n', '<leader>pw', [[<cmd>lua require('telescope.builtin').grep_string({search = vim.fn.expand("<cword>")})<CR>]], opts)
+map('n', '<leader>pw', [[<cmd>lua require('telescope.builtin').grep_string({search = vim.fn.expand("<cword>")})<CR>]],
+    opts)
 -- " grep line
 map('n', '<leader>pl', [[<cmd>lua require('telescope.builtin').grep_string()<CR>]], opts)
 
