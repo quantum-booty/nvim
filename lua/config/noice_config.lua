8c157757fb849f97feff434628422a20d8e70ddf
Henry Wang <henrywang97@gmail.com>
2022-12-28 17:49:51 +0000

    disable noice

--- lua/config/noice_config.lua
+++ lua/config/noice_config.lua
@@ -63,33 +63,33 @@
 --     }
 -- )
 
-
-require("noice").setup({
-    lsp = {
-        progress = {
-            enabled = true,
-        },
-        hover = {
-            enabled = false,
-        },
-        signature = {
-            enabled = false,
-        },
-        -- override = {
-        --     -- override the default lsp markdown formatter with Noice
-        --     ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-        --     -- override the lsp markdown formatter with Noice
-        --     ["vim.lsp.util.stylize_markdown"] = true,
-        --     -- override cmp documentation with Noice (needs the other options to work)
-        --     ["cmp.entry.get_documentation"] = true,
-        -- },
-    },
-    -- you can enable a preset for easier configuration
-    presets = {
-        bottom_search = false, -- use a classic bottom cmdline for search
-        command_palette = true, -- position the cmdline and popupmenu together
-        long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = false, -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = false, -- add a border to hover docs and signature help
-    },
-})
+--
+-- require("noice").setup({
+--     lsp = {
+--         progress = {
+--             enabled = true,
+--         },
+--         hover = {
+--             enabled = false,
+--         },
+--         signature = {
+--             enabled = false,
+--         },
+--         -- override = {
+--         --     -- override the default lsp markdown formatter with Noice
+--         --     ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+--         --     -- override the lsp markdown formatter with Noice
+--         --     ["vim.lsp.util.stylize_markdown"] = true,
+--         --     -- override cmp documentation with Noice (needs the other options to work)
+--         --     ["cmp.entry.get_documentation"] = true,
+--         -- },
+--     },
+--     -- you can enable a preset for easier configuration
+--     presets = {
+--         bottom_search = false, -- use a classic bottom cmdline for search
+--         command_palette = true, -- position the cmdline and popupmenu together
+--         long_message_to_split = true, -- long messages will be sent to a split
+--         inc_rename = false, -- enables an input dialog for inc-rename.nvim
+--         lsp_doc_border = false, -- add a border to hover docs and signature help
+--     },
+-- })
