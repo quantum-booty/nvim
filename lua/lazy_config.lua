d2a2a99ce783b5409dc28eb1ee60fae2571ac59f
Henry Wang <henrywang97@gmail.com>
2022-12-30 18:51:44 +0000

    disable builtin plugins

--- lua/lazy_config.lua
+++ lua/lazy_config.lua
@@ -13,27 +13,40 @@ end
 vim.opt.runtimepath:prepend(lazypath)
 
 require("lazy").setup("plugins", {
-  -- defaults = { lazy = true },
-  install = { missing = true },
-  checker = { enabled = false },
-  diff = {
-    cmd = "terminal_git",
-  },
-  performance = {
-    cache = {
-      enabled = true,
+    -- defaults = { lazy = true },
+    install = { missing = true },
+    checker = { enabled = false },
+    diff = {
+        cmd = "terminal_git",
     },
-    rtp = {
-      disabled_plugins = {
-        "gzip",
-        "matchit",
-        "matchparen",
-        "netrwPlugin",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
-      },
+    performance = {
+        cache = {
+            enabled = true,
+        },
+        rtp = {
+            disabled_plugins = {
+                "matchparen",
+                "tohtml",
+                "tutor",
+                "netrw",
+                "netrwPlugin",
+                "netrwSettings",
+                "netrwFileHandlers",
+                "gzip",
+                "zip",
+                "zipPlugin",
+                "tar",
+                "tarPlugin",
+                "getscript",
+                "getscriptPlugin",
+                "vimball",
+                "vimballPlugin",
+                "2html_plugin",
+                "logipat",
+                "rrhelper",
+                "spellfile_plugin",
+                "matchit"
+            },
+        },
     },
-  },
 })
--- lua/settings.lua
+++ lua/settings.lua
@@ -14,33 +14,6 @@ else
     vim.g.python3_host_prog = os.getenv('PYENV_ROOT')..'/versions/neovim3/bin/python'
 end
 
--- disable unused build in plugins to improve start-up time
-local disabled_built_ins = {
-    "netrw",
-    "netrwPlugin",
-    "netrwSettings",
-    "netrwFileHandlers",
-    "gzip",
-    "zip",
-    "zipPlugin",
-    "tar",
-    "tarPlugin",
-    "getscript",
-    "getscriptPlugin",
-    "vimball",
-    "vimballPlugin",
-    "2html_plugin",
-    "logipat",
-    "rrhelper",
-    "spellfile_plugin",
-    "matchit"
-}
-for _, plugin in pairs(disabled_built_ins) do
-    vim.g["loaded_" .. plugin] = 1
-end
-
-
-
 vim.cmd([[
     " Hide ~ for empty buffer line
     let &fcs='eob: '
