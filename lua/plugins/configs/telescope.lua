9fe47c39fecf7a2c675f1cd27e21e1e933f4fd36
Henry Wang <henrywang97@gmail.com>
2022-12-30 16:03:51 +0000

    debloat telescope

--- lua/plugins/configs/telescope.lua
+++ lua/plugins/configs/telescope.lua
@@ -1,35 +1,8 @@
 local map = require('utils').map
 local opts = { noremap = true, silent = true }
 
-local actions = require("telescope.actions")
 local trouble = require("trouble.providers.telescope")
-
 local telescope = require("telescope")
--- local fb_actions = require "telescope".extensions.file_browser.actions
-
-local open_in_nvim_tree = function(prompt_bufnr)
-    local action_state = require "telescope.actions.state"
-    local Path = require "plenary.path"
-
-    local entry = action_state.get_selected_entry()[1]
-    local entry_path = Path:new(entry):parent():absolute()
-    actions.close(prompt_bufnr)
-    entry_path = Path:new(entry):parent():absolute()
-    entry_path = entry_path:gsub("\\", "\\\\")
-
-    vim.cmd("NvimTreeClose")
-    vim.cmd("NvimTreeOpen " .. entry_path)
-    -- vim.cmd("Neotree dir=" .. entry_path)
-
-    local file_name = nil
-    for s in string.gmatch(entry, "[^/]+") do
-        file_name = s
-    end
-
-    vim.cmd("?" .. file_name)
-end
-
-
 
 telescope.setup {
     defaults = {
@@ -51,11 +24,9 @@ telescope.setup {
         mappings = {
             i = {
                 ["<c-t>"] = trouble.open_with_trouble,
-                ["<c-s>"] = open_in_nvim_tree,
             },
             n = {
                 ["<c-t>"] = trouble.open_with_trouble,
-                ["<c-s>"] = open_in_nvim_tree,
             },
         },
     },
@@ -68,33 +39,6 @@ telescope.setup {
         lsp_definitions = { initial_mode = 'normal', theme = "ivy", show_line = false },
         lsp_implementations = { initial_mode = 'normal', theme = "ivy", show_line = false },
         lsp_references = { initial_mode = 'normal', theme = "ivy", show_line = false },
-    },
-    extensions = {
-        -- file_browser = {
-        --     -- initial_mode='normal',
-        --     -- files = false,
-        --     theme = "ivy",
-        --     mappings = {
-        --         ["i"] = {
-        --             -- your custom insert mode mappings
-        --         },
-        --         ["n"] = {
-        --             ["n"] = fb_actions.create,
-        --             ["r"] = fb_actions.rename,
-        --             ["v"] = fb_actions.move,
-        --             ["y"] = fb_actions.copy,
-        --             ["x"] = fb_actions.remove,
-        --             ["o"] = fb_actions.open,
-        --             ["h"] = fb_actions.goto_parent_dir,
-        --             ["~"] = fb_actions.goto_home_dir,
-        --             ["w"] = fb_actions.goto_cwd,
-        --             ["t"] = fb_actions.change_cwd,
-        --             ["f"] = fb_actions.toggle_browser,
-        --             ["."] = fb_actions.toggle_hidden,
-        --             ["m"] = fb_actions.toggle_all,
-        --         },
-        --     },
-        -- },
     },
 }
 require('telescope').load_extension('zf-native')
@@ -120,18 +64,16 @@ map('n', '<leader>pp', ':Telescope projects<CR>', opts)
 -- " --- File Pickers
 -- " Fuzzy find over git files in your directory
 vim.keymap.set('n', '<C-p>', project_files, opts)
+vim.keymap.set('n', '<leader>pf', require('telescope.builtin').find_files, opts)
 vim.keymap.set('n', '<leader>ps', require('telescope.builtin').git_status, opts)
 -- " grep typed string
 map('n', '<leader>pg', [[<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>]], opts)
 -- " grep cursor word
 map('n', '<leader>pw', [[<cmd>lua require('telescope.builtin').grep_string({search = vim.fn.expand("<cword>")})<CR>]],
     opts)
--- " grep line
-map('n', '<leader>pl', [[<cmd>lua require('telescope.builtin').grep_string()<CR>]], opts)
 
 vim.keymap.set('n', '<leader>pb', require('telescope.builtin').current_buffer_fuzzy_find, opts)
 vim.keymap.set('n', '<leader>po', require('telescope.builtin').oldfiles, opts)
-
 
 
 -- " --- Vim Pickers
@@ -144,27 +86,3 @@ map('n', '<leader>ph', [[<cmd>Telescope help_tags<cr>]], opts)
 
 -- " Colour schemes
 map('n', '<leader>pc', [[<cmd>lua require('telescope.builtin').colorscheme()<CR>]], opts)
--- " Key maps
-map('n', '<leader>pk', [[<cmd>lua require('telescope.builtin').keymaps()<CR>]], opts)
-
-
--- -- " " --- git pickers
--- -- " " commits
--- map('n', '<leader>pgc', [[<cmd>lua require('telescope.builtin').git_commits()<CR>]], opts)
--- -- " " buffer git commits
--- map('n', '<leader>pgbc', [[<cmd>lua require('telescope.builtin').git_bcommits()<CR>]], opts)
--- -- " " branches
--- map('n', '<leader>pgb', [[<cmd>lua require('telescope.builtin').git_branches()<CR>]], opts)
--- -- " " status
--- map('n', '<leader>pgs', [[<cmd>lua require('telescope.builtin').git_status()<CR>]], opts)
--- -- "
-
--- file_browser
--- require("telescope").load_extension "file_browser"
--- map('n', '<leader>pv', ':Telescope file_browser<CR>', opts)
---
---   local fb = require("telescope").extensions.file_browser.file_browser
--- path = "%:p:h"
---
---
---
