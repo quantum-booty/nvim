93f316b269f463f3610887cd0553ddb60e1ae9c0
Henry Wang <henrywang97@gmail.com>
2022-12-31 16:08:04 +0000

    lazy unstable

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,7 +2,7 @@ local not_windows = require('utils').not_windows
 local is_windows = require('utils').is_windows
 
 return {
-    { 'folke/lazy.nvim', tag = 'stable' },
+    'folke/lazy.nvim',
     'nvim-lua/plenary.nvim',
     'nvim-lua/popup.nvim',
     'kyazdani42/nvim-web-devicons',
