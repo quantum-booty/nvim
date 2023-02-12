89eafbe94d852902df4d864b1eb2da8d01bc3a5e
Henry Wang <henrywang97@gmail.com>
2023-02-12 12:31:16 +0000

    update neozoom

--- lua/plugins/configs/neozoom.lua
+++ lua/plugins/configs/neozoom.lua
@@ -1,11 +1,25 @@
-require('neo-zoom').setup { -- use the defaults or UNCOMMENT and change any one to overwrite
-    -- left_ratio = 0.0,
-    -- top_ratio = 0.0,
-    width_ratio = 1.,
-    height_ratio = 1.,
-    border = 'none',
-    -- exclude_filetype = {
-    --   'fzf', 'qf', 'dashboard'
-    -- }
+require('neo-zoom').setup {
+    winopts = {
+        offset = {
+            width = 0.99,
+            height = 0.95,
+        },
+        border = 'none',
+    },
+    exclude_buftypes = { 'terminal' },
+    presets = {
+        {
+            filetypes = { 'dapui_.*', 'dap-repl' },
+            config = {
+                top = 0.25,
+                left = 0.6,
+                width = 0.4,
+                height = 0.65,
+            },
+            callbacks = {
+                function() vim.wo.wrap = true end,
+            },
+        },
+    },
 }
 vim.keymap.set('n', '<Del>', require("neo-zoom").neo_zoom, { silent = true, nowait = true })
