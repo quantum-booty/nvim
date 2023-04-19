c223e6bb07e0ca6b19ed1927ed2243e2ca4a1297
Henry Wang <henrywang97@gmail.com>
2023-04-19 18:04:24 +0100

    update neorg config

--- lua/plugins/configs/neorg.lua
+++ lua/plugins/configs/neorg.lua
@@ -2,7 +2,7 @@ require('neorg').setup {
     lazy_loading = true,
     load = {
         ['core.defaults'] = {},	-- Tells neorg to load the module called core.defaults with no extra data
-        ['core.norg.concealer'] = {
+        ['core.concealer'] = {
             config = {
                 icon_preset = 'diamond'
             }
